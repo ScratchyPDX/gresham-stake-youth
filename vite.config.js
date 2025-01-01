c57864d2a7265b5b631cd31f0962b67f6189d3f7
LT-SCRATCHY-U9\wardd <ward.david@live.com>
2024-12-31 19:18:22 -0800

    Add missing page

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,7 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product1: resolve(__dirname, "src/product_pages/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html"),
-        admin: resolve(__dirname, "src/admin/index.html")
+        main: resolve(__dirname, "src/index.html")
       }
     }
   }
